d05c6b24a2d138b6a870aea5852b27ee76d47d2e
Arvin Foroutan <arvin.foroutan@gmail.com>
2021-09-03 06:27:29 -0700

    [5.13] arch-patches-v5 -> arch-patches-v6

--- build_kernel.sh
+++ build_kernel.sh
@@ -323,9 +323,8 @@ elif [ ${KERNEL_BASE_VER} == "5.13" ]; then
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/alsa-patches-v2/*.patch .;
     patch -p1 < ./0001-alsa-patches.patch;
     echo "*** Copying and applying arch patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/arch-patches-v5-sep/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/arch-patches-v6/*.patch .;
     patch -p1 < ./0001-ZEN-Add-sysctl-and-CONFIG-to-disallow-unprivileged-C.patch;
-    patch -p1 < ./0002-Bluetooth-btusb-check-conditions-before-enabling-USB.patch;
     echo "*** Copying and applying bbr2 patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/bbr2-patches-v2/*.patch .;
     patch -p1 < ./0001-bbr2-patches.patch;
