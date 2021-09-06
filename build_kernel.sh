db8d0a6e53da441476df3cf32b8f891026ff9319
Arvin Foroutan <arvin.foroutan@gmail.com>
2021-09-06 12:58:52 -0700

    [5.14] aufs-2021-08-09 -> aufs-2021-09-06

--- build_kernel.sh
+++ build_kernel.sh
@@ -177,7 +177,7 @@ if [ ${KERNEL_BASE_VER} == "5.14" ]; then
     patch -p1 < ./0001-ZEN-Add-sysctl-and-CONFIG-to-disallow-unprivileged-C.patch;
     echo "*** Copying and applying aufs patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/aufs-patches/*.patch .;
-    patch -p1 < ./0001-aufs-20210809.patch;
+    patch -p1 < ./0001-aufs-20210906.patch;
     echo "*** Copying and applying bbr2 patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/bbr2-patches/*.patch .;
     patch -p1 < ./0001-bbr2-5.14-introduce-BBRv2.patch;
