f8663e739679fcf5b76dfae7eadec1adafb54f1f
Arvin Foroutan <arvin.foroutan@gmail.com>
2021-09-20 22:34:56 -0700

    [5.14] ntfs3-patches-v7 -> ntfs3-patches-v8

--- build_kernel.sh
+++ build_kernel.sh
@@ -345,7 +345,7 @@ elif [ ${KERNEL_BASE_VER} == "5.14" ]; then
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/lrng-patches-v2/*.patch .;
     patch -p1 < ./0001-lrng-patches.patch;
     echo "*** Copying and applying ntfs3 patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/ntfs3-patches-v7/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/ntfs3-patches-v8/*.patch .;
     patch -p1 < ./0001-ntfs3-patches.patch;
     echo "*** Copying and applying pf patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/pf-patches-v5-sep/*.patch .;
