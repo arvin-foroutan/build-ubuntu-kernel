eab15378e1ec1a3a263b8e75ef205991101c059b
Arvin Foroutan <arvin.foroutan@gmail.com>
2021-10-12 08:57:51 -0700

    [5.14] zstd-upstream-patches-v4 -> zstd-upstream-patches-v6

--- build_kernel.sh
+++ build_kernel.sh
@@ -277,7 +277,7 @@ if [ ${KERNEL_BASE_VER} == "5.15" ]; then   # Latest mainline, in -rc right now
     cp -v ${LUCJAN_PATCH_PATH}/5.14/zstd-patches-v2/*.patch .;
     patch -p1 < ./0001-zstd-patches.patch;
     echo "*** Copying and applying zstd upstream patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/5.14/zstd-upstream-patches-v4/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/5.14/zstd-upstream-patches-v6/*.patch .;
     patch -p1 < ./0001-zstd-upstream-patches.patch;
     # Misc / Tweaks
     sed -i 's/sched_nr_migrate = 32/sched_nr_migrate = 256/g' ./kernel/sched/core.c;
