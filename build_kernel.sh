4c534745e13aed3eb9fdca3dc530d1fd31217c38
Arvin Foroutan <arvin.foroutan@gmail.com>
2021-10-27 21:30:52 -0700

    [5.14 / 5.15] zstd-upstream-patches-v6 -> zstd-upstream-patches-v7

--- build_kernel.sh
+++ build_kernel.sh
@@ -304,7 +304,7 @@ if [ ${KERNEL_BASE_VER} == "5.15" ]; then   # Latest mainline, in -rc right now
     cp -v ${LUCJAN_PATCH_PATH}/5.14/zstd-patches-v2/*.patch .;
     patch -p1 < ./0001-zstd-patches.patch;
     echo "*** Copying and applying zstd upstream patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/5.14/zstd-upstream-patches-v6/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/5.14/zstd-upstream-patches-v7/*.patch .;
     patch -p1 < ./0001-zstd-upstream-patches.patch;
     # Misc / Tweaks
     sed -i 's/sched_nr_migrate = 32/sched_nr_migrate = 256/g' ./kernel/sched/core.c;
@@ -485,7 +485,7 @@ elif [ ${KERNEL_BASE_VER} == "5.14" ]; then # Latest stable kernel
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/zstd-patches-v2/*.patch .;
     patch -p1 < ./0001-zstd-patches.patch;
     echo "*** Copying and applying zstd upstream patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/zstd-upstream-patches-v6/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/zstd-upstream-patches-v7/*.patch .;
     patch -p1 < ./0001-zstd-upstream-patches.patch;
     # Misc / Tweaks
     sed -i 's/sched_nr_migrate = 32/sched_nr_migrate = 256/g' ./kernel/sched/core.c;
