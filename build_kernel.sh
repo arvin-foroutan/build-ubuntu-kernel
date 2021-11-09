a0a808abb7ce10f36be25e14722d324de633f282
Arvin Foroutan <arvin.foroutan@gmail.com>
2021-11-09 09:39:06 -0800

    [5.15] Added latest -rt (real-time) patch: 5.15-rc7-rt15 -> 5.15-rt17
    The latest -rt patch for 5.15 (5.15-rt17) is as of 02-Nov-2021 and was pulled from: https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.15/
    
    To build 5.15-rt17:
    
    KERNEL_TYPE=rt KERNEL_BASE_VER=5.15 KERNEL_PATCH_VER=5.15 KERNEL_SUB_VER=051500 ./build_kernel.sh
    
    Optional variables you can also pass in if applicable:
    
    AMDGPU_BUILTIN=yes
    VBOX_SUPPORT=yes

--- build_kernel.sh
+++ build_kernel.sh
@@ -172,8 +172,8 @@ fi
 if [ ${KERNEL_TYPE} == "rt" ]; then
     echo "*** Copying and applying rt patches... ✓";
     if [ ${KERNEL_BASE_VER} == "5.15" ]; then
-        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.15-rc7-rt15.patch .;
-        patch -p1 < ./patch-5.15-rc7-rt15.patch;
+        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.15-rt17.patch .;
+        patch -p1 < ./patch-5.15-rt17.patch;
     elif [ ${KERNEL_BASE_VER} == "5.14" ]; then
         cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.14.2-rt21.patch .;
         patch -p1 < ./patch-5.14.2-rt21.patch;
