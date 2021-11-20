4337a6d078e310a6bf1a511b8289eec080be9067
Arvin Foroutan <arvin.foroutan@gmail.com>
2021-11-19 17:49:40 -0800

    [5.10] Added latest -rt (real-time) patch: 5.10.73-rt54 -> 5.10.78-rt55
    The latest -rt patch for 5.10 (5.10.78-rt55) is as of 15-Nov-2021 and was pulled from: https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.10/
    
    To build 5.10.78-rt55:
    
    $ KERNEL_TYPE=rt KERNEL_BASE_VER=5.10 KERNEL_PATCH_VER=5.10.78 KERNEL_SUB_VER=051078 ./build_kernel.sh
    
    Optional variables you can also pass in if applicable:
    
    AMDGPU_BUILTIN=yes
    VBOX_SUPPORT=yes

--- build_kernel.sh
+++ build_kernel.sh
@@ -187,8 +187,8 @@ if [ ${KERNEL_TYPE} == "rt" ]; then
         cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.13-rt1.patch .;
         patch -p1 < ./patch-5.13-rt1.patch;
     elif [ ${KERNEL_BASE_VER} == "5.10" ]; then
-        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.10.73-rt54.patch .;
-        patch -p1 < ./patch-5.10.73-rt54.patch;
+        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.10.78-rt55.patch .;
+        patch -p1 < ./patch-5.10.78-rt55.patch;
     elif [ ${KERNEL_BASE_VER} == "5.4" ]; then
         cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.4.154-rt65.patch .;
         patch -p1 < ./patch-5.4.154-rt65.patch;
