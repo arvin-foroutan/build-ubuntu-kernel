89e0ed84659f51693d91aa5ac38318d64a07bdb8
Arvin Foroutan <arvin.foroutan@gmail.com>
2021-12-17 17:39:38 -0800

    Build the newly released 5.15.10 by default (i.e, ./build_kernel.sh)
    A few notes:
    
    - 5.15 is now the default kernel being compiled when you run ./build_kernel.sh
    - 5.14 and 5.13 are both EOL (end of life) so they won't receive security updates and are no longer recommended, but are still available for building
    - 5.10 LTS (supported until 2026) is available in case you need it for any reason
    - 5.4 LTS (supported until 2025) is also available and is a very solid kernel
    
    To view the changelog:
    
    Web Browser: https://cdn.kernel.org/pub/linux/kernel/v5.x/ChangeLog-5.15.10
    
    Terminal: $ wget -q -O - https://cdn.kernel.org/pub/linux/kernel/v5.x/ChangeLog-5.15.10 | grep -A2 'Date:' | grep -v 'Date:\|^$\|^--' | sort | less
    
    To build 5.15.10:
    
    $ ./build_kernel.sh
    
    To build the other newly released versions:
    
    5.10.87 LTS
    
    $ KERNEL_BASE_VER=5.10 KERNEL_PATCH_VER=5.10.87 KERNEL_SUB_VER=051087 ./build_kernel.sh
    
    5.4.167 LTS
    
    $ KERNEL_BASE_VER=5.4 KERNEL_PATCH_VER=5.4.167 KERNEL_SUB_VER=0504167 ./build_kernel.sh
    
    5.14.21 (EOL - end of life)
    
    $ KERNEL_BASE_VER=5.14 KERNEL_PATCH_VER=5.14.21 KERNEL_SUB_VER=051421 ./build_kernel.sh
    
    5.13.19 (EOL - end of life)
    
    $ KERNEL_BASE_VER=5.13 KERNEL_PATCH_VER=5.13.19 KERNEL_SUB_VER=051319 ./build_kernel.sh
    
    which will build the following:
    
    A) Low-Latency Preemptive Kernel
    B) 1000HZ timer, idle tickless
    C) Compiled with gcc and 'Generic x86/64' optimizations
    
    Note: 5.4 and 5.10 will default to "Native Optimizations" (-march=native), the others will need to be set manually in the config when you run the script. There's also a more generic "Core 2" option, but you should select your specific processor from the dropdown if you see it, or, "Intel Native" or "AMD Native" to create the fastest "happy" path for the compiled kernel.
    
    To find this setting: In the config, select Processor type and features ---> and then look for Processor family. (You can use forward slash ( / ) to search for anything in the config. When asked by the script if you want to edit "low latency" say "n" as the "generic" kernel built by the script *is* the low-latency kernel.
    
    ---
    
    Optional variables you can pass to the script that may be applicable to you:
    
    AMDGPU_BUILTIN=yes
    VBOX_SUPPORT=yes
    
    Example usage to use the above variables:
    
    AMDGPU_BUILTIN=yes VBOX_SUPPORT=yes ./build_kernel.sh
    
    The above command will build the 5.15 kernel with the amdgpu kernel module built-in (faster boot-up and overall smoothness since it's baked in and doesn't need to be loaded in) and VirtualBox support for running virtual machines. See script for more configurable overrides.

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,8 +7,8 @@
 set -euo pipefail
 
 KERNEL_BASE_VER=${KERNEL_BASE_VER:-"5.15"}
-KERNEL_PATCH_VER=${KERNEL_PATCH_VER:-"5.15.8"}
-KERNEL_SUB_VER=${KERNEL_SUB_VER:-"051508"}
+KERNEL_PATCH_VER=${KERNEL_PATCH_VER:-"5.15.10"}
+KERNEL_SUB_VER=${KERNEL_SUB_VER:-"051510"}
 KERNEL_TYPE=${KERNEL_TYPE:-"idle"} # idle, full, rt
 KERNEL_SCHEDULER=${KERNEL_SCHEDULER:-"cfs"} # cfs, cacule
 KERNEL_VERSION_LABEL=${KERNEL_VERSION_LABEL:-"custom"}
@@ -1382,11 +1382,11 @@ if [ ${KERNEL_SCHEDULER} == "cacule" ] && [ "${KERNEL_TYPE}" != "rt" ]; then
 fi
 
 # Examples:
-# 5.15.8-051508+customidle-generic
-# 5.15.8-051508+customfull-generic
-# 5.15.8-051508+customrt-generic
+# 5.15.10-051510+customidle-generic
+# 5.15.10-051510+customfull-generic
+# 5.15.10-051510+customrt-generic
 # Note: A hyphen between label and type (e.g. customidle -> custom-idle) causes problems with some parsers
-# Because the final version name becomes: 5.15.8-051508+custom-idle-generic, so just keep it combined
+# Because the final version name becomes: 5.15.10-051510+custom-idle-generic, so just keep it combined
 echo "*** Updating version in changelog (necessary for Ubuntu)... ✓";
 sed -i "s/${KERNEL_SUB_VER}/${KERNEL_SUB_VER}+${KERNEL_VERSION_LABEL}${KERNEL_TYPE}/g" ./debian.master/changelog;
 
@@ -1527,7 +1527,7 @@ rm -rf ${KERNEL_BUILD_DIR};
 # Also note: Running 'sudo update-grub2' will list your installed kernels,
 # and you can manually delete the ones that have uninstall as time goes on.
 #
-# To uninstall a kernel: $ sudo apt purge *5.15.8-051508+customidle-generic*
+# To uninstall a kernel: $ sudo apt purge *5.15.10-051510+customidle-generic*
 # However, you still need to manually remove the old ones that build up below.
 echo "ls -alh /usr/src"
 ls -alh /usr/src;
