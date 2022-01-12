01e9cacf75e9e78ab9efcdc808c2d1141b14cc66
Arvin Foroutan <arvin.foroutan@gmail.com>
2022-01-12 09:55:35 -0800

    [5.16] Added latest -rt (real-time) patch: 5.16-rc8-rt13 -> 5.16-rt14
    The latest -rt patch for 5.16 (5.16-rt14) is as of 11-Jan-2022 and was pulled from: https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.16/
    
    To build 5.16-rt14:
    
    $ KERNEL_TYPE=rt KERNEL_BASE_VER=5.16 KERNEL_PATCH_VER=5.16 KERNEL_SUB_VER=051600 ./build_kernel.sh
    
    Optional variables you can also pass in if applicable:
    
    AMDGPU_BUILTIN=yes
    VBOX_SUPPORT=yes

--- build_kernel.sh
+++ build_kernel.sh
@@ -178,8 +178,8 @@ fi
 if [ ${KERNEL_TYPE} == "rt" ]; then
     echo "*** Copying and applying rt patches... ✓";
     if [ ${KERNEL_BASE_VER} == "5.16" ]; then
-        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.16-rc8-rt13.patch .;
-        patch -p1 < ./patch-5.16-rc8-rt13.patch;
+        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.16-rt14.patch .;
+        patch -p1 < ./patch-5.16-rt14.patch;
     elif [ ${KERNEL_BASE_VER} == "5.15" ]; then
         cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.15.13-rt26.patch .;
         patch -p1 < ./patch-5.15.13-rt26.patch;
