41371a0c146d268f4c905e7cee4d0fb9e9fc212e
Arvin Foroutan <arvin.foroutan@gmail.com>
2022-02-04 08:10:27 -0800

    [5.16 / 5.17] fixes-miscellaneous-v3 -> fixes-miscellaneous-v4

--- build_kernel.sh
+++ build_kernel.sh
@@ -239,7 +239,7 @@ if [ ${KERNEL_BASE_VER} == "5.17" ]; then   # Latest rc, in development
     patch -p1 < ./0003-init-Kconfig-add-O1-flag.patch;
     patch -p1 < ./0004-Makefile-Turn-off-loop-vectorization-for-GCC-O3-opti.patch;
     echo "*** Copying and applying fixes misc patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/5.16/fixes-miscellaneous-v2-sep/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/5.16/fixes-miscellaneous-v4-sep/*.patch .;
     patch -p1 < ./0001-net-sched-allow-configuring-cake-qdisc-as-default.patch;
     patch -p1 < ./0002-infiniband-Fix-__read_overflow2-error-with-O3-inlini.patch;
     patch -p1 < ./0003-pci-Enable-overrides-for-missing-ACS-capabilities.patch;
@@ -247,7 +247,7 @@ if [ ${KERNEL_BASE_VER} == "5.17" ]; then   # Latest rc, in development
     patch -p1 < ./0007-i2c-busses-Add-SMBus-capability-to-work-with-OpenRGB.patch;
     patch -p1 < ./0008-fm-5.16-port-mm-kswapd-patches.patch;
     patch -p1 < ./0009-Disable-stack-conservation-for-GCC.patch;
-    patch -p1 < ./0014-openrgb-Deduplicate-piix4-setup-for-HUDSON2-KERNCZ-S.patch;
+    patch -p1 < ./0016-kernel-cpu.c-fix-init_cpu_online.patch;
     echo "*** Copying and applying lqx patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/5.16/lqx-patches/*.patch .;
     patch -p1 < ./0001-lqx-patches.patch;
@@ -365,7 +365,7 @@ elif [ ${KERNEL_BASE_VER} == "5.16" ]; then # Latest stable
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/f2fs-patches-v2/*.patch .;
     patch -p1 < ./0001-f2fs-patches.patch;
     echo "*** Copying and applying fixes misc patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/fixes-miscellaneous-v3-sep/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/fixes-miscellaneous-v4-sep/*.patch .;
     patch -p1 < ./0001-net-sched-allow-configuring-cake-qdisc-as-default.patch;
     patch -p1 < ./0002-infiniband-Fix-__read_overflow2-error-with-O3-inlini.patch;
     patch -p1 < ./0003-pci-Enable-overrides-for-missing-ACS-capabilities.patch;
@@ -380,6 +380,7 @@ elif [ ${KERNEL_BASE_VER} == "5.16" ]; then # Latest stable
     patch -p1 < ./0013-xfs-check-sb_meta_uuid-for-dabuf-buffer-recovery.patch;
     patch -p1 < ./0014-openrgb-Deduplicate-piix4-setup-for-HUDSON2-KERNCZ-S.patch;
     patch -p1 < ./0015-iomap-Address-soft-lockup-in-iomap_finish_ioend.patch;
+    patch -p1 < ./0016-kernel-cpu.c-fix-init_cpu_online.patch;
     echo "*** Copying an applying hwmon patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/hwmon-patches-v7/*.patch .;
     patch -p1 < ./0001-hwmon-patches.patch;
