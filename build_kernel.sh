bea92fa3aec245d55e661c6d997d1bfc582dbb9c
Arvin Foroutan <arvin.foroutan@gmail.com>
2022-02-23 09:35:23 -0800

    [5.16] cpufreq-patches-v3 -> cpufreq-patches-v4

--- build_kernel.sh
+++ build_kernel.sh
@@ -360,7 +360,7 @@ elif [ ${KERNEL_BASE_VER} == "5.16" ]; then # Latest stable
     patch -p1 < ./0003-init-Kconfig-add-O1-flag.patch;
     patch -p1 < ./0004-Makefile-Turn-off-loop-vectorization-for-GCC-O3-opti.patch;
     echo "*** Copying and applying cpufreq patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/cpufreq-patches-v3/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/cpufreq-patches-v4/*.patch .;
     patch -p1 < ./0001-cpufreq-patches.patch;
     echo "*** Copying and applying f2fs patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/f2fs-patches-v2/*.patch .;
