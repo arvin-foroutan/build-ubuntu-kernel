0043a4a3700a7c13df2057cf105cbd294e1c6539
Arvin Foroutan <arvin.foroutan@gmail.com>
2022-02-24 20:22:47 -0800

    [5.16] btrfs-patches-v6 -> btrfs-patches-v7

--- build_kernel.sh
+++ build_kernel.sh
@@ -323,7 +323,7 @@ elif [ ${KERNEL_BASE_VER} == "5.16" ]; then # Latest stable
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/block-patches/*.patch .;
     patch -p1 < ./0001-block-patches.patch;
     echo "*** Copying and applying btrfs patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/btrfs-patches-v6/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/btrfs-patches-v7/*.patch .;
     patch -p1 < ./0001-btrfs-patches.patch;
     echo "*** Copying and applying clearlinux patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/clearlinux-patches-sep/*.patch .;
