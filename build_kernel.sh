d99d825e3d79a05717a2e96881b01e4b74f744c5
Arvin Foroutan <arvin.foroutan@gmail.com>
2022-03-28 08:14:33 -0700

    [5.17] Add latest rt patch: 5.17-rc6-rt11 -> 5.17-rt15
    The latest real-time patch for 5.17 (5.17-rt15) is as of 21-Mar-2022 and was pulled from: https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.17/
    
    To build 5.17-rt15:
    
    $ KERNEL_TYPE=rt KERNEL_BASE_VER=5.17 KERNEL_PATCH_VER=5.17 KERNEL_SUB_VER=051700 ./build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -176,8 +176,8 @@ fi
 if [ ${KERNEL_TYPE} == "rt" ]; then
     echo "*** Copying and applying rt patches... ✓";
     if [ ${KERNEL_BASE_VER} == "5.17" ]; then
-        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.17-rc6-rt11.patch .;
-        patch -p1 < ./patch-5.17-rc6-rt11.patch;
+        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.17-rt15.patch .;
+        patch -p1 < ./patch-5.17-rt15.patch;
     elif [ ${KERNEL_BASE_VER} == "5.16" ]; then
         cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.16.1-rt17.patch .;
         patch -p1 < ./patch-5.16.1-rt17.patch;
