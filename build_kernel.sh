24d944f15e837fd2886f7747554f6ad1b2940b72
Arvin Foroutan <arvin.foroutan@gmail.com>
2022-04-11 11:48:24 -0700

    [5.10] Add latest rt patch: 5.10.106-rt64 -> 5.10.109-rt65
    The latest real-time patch for 5.10 (5.10.109-rt65) is as of 07-Apr-2022 and was pulled from: https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.10/
    
    To build 5.10.109-rt65:
    
    $ KERNEL_TYPE=rt KERNEL_BASE_VER=5.10 KERNEL_PATCH_VER=5.10.109 KERNEL_SUB_VER=0510109 ./build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -191,8 +191,8 @@ if [ ${KERNEL_TYPE} == "rt" ]; then
         cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.13-rt1.patch .;
         patch -p1 < ./patch-5.13-rt1.patch;
     elif [ ${KERNEL_BASE_VER} == "5.10" ]; then
-        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.10.106-rt64.patch .;
-        patch -p1 < ./patch-5.10.106-rt64.patch;
+        cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.10.109-rt65.patch .;
+        patch -p1 < ./patch-5.10.109-rt65.patch;
     elif [ ${KERNEL_BASE_VER} == "5.4" ]; then
         cp -v ${CUSTOM_PATCH_PATH}/rt/${KERNEL_BASE_VER}/patch-5.4.182-rt72-rc1.patch .;
         patch -p1 < ./patch-5.4.182-rt72-rc1.patch;
