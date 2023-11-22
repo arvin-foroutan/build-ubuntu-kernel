17b314dc0a71e03337fd1c15e55585b515c9a52a
Arvin Foroutan <arvin.foroutan@gmail.com>
2023-11-21 19:49:05 -0800

    6.6.1 -> 6.6.2

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,8 +6,8 @@ set -euo pipefail
 
 KERNEL_MAJOR_VER=${KERNEL_MAJOR_VER:-"6"}
 KERNEL_BASE_VER=${KERNEL_BASE_VER:-"6.6"}
-KERNEL_PATCH_VER=${KERNEL_PATCH_VER:-"6.6.1"}
-KERNEL_SUB_VER=${KERNEL_SUB_VER:-"060601"}
+KERNEL_PATCH_VER=${KERNEL_PATCH_VER:-"6.6.2"}
+KERNEL_SUB_VER=${KERNEL_SUB_VER:-"060602"}
 KERNEL_TYPE=${KERNEL_TYPE:-"idle"}
 KERNEL_SCHEDULER=${KERNEL_SCHEDULER:-"cfs"}
 KERNEL_VERSION_LABEL=${KERNEL_VERSION_LABEL:-"custom"}
@@ -217,7 +217,7 @@ if [ ${KERNEL_BASE_VER} == "6.6" ]; then    # Latest mainline
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/futex-patches/*.patch .;
     patch -p1 < ./0001-futex-6.6-Add-entry-point-for-FUTEX_WAIT_MULTIPLE-op.patch;
     echo "*** Copying and applying fixes misc patches.. ✓";
-    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/fixes-miscellaneous/*.patch .;
+    cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/fixes-miscellaneous-v2/*.patch .;
     patch -p1 < ./0001-fixes-miscellaneous.patch;
     echo "*** Copying and applying net patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/net-patches-all/*.patch .;
@@ -893,11 +893,11 @@ elif [ ${KERNEL_BASE_VER} == "5.4" ]; then  # LTS kernel, supported until 2025
 fi
 
 # Examples:
-# 6.6.1-060601+customidle-generic
-# 6.6.1-060601+customfull-generic
-# 6.6.1-060601+customrt-generic
+# 6.6.2-060602+customidle-generic
+# 6.6.2-060602+customfull-generic
+# 6.6.2-060602+customrt-generic
 # Note: A hyphen between label and type (e.g. customidle -> custom-idle) causes problems with some parsers
-# Because the final version name becomes: 6.6.1-060601+custom-idle-generic, so just keep it combined
+# Because the final version name becomes: 6.6.2-060602+custom-idle-generic, so just keep it combined
 echo "*** Updating version in changelog (necessary for Ubuntu)... ✓";
 sed -i "s/${KERNEL_SUB_VER}/${KERNEL_SUB_VER}+${KERNEL_VERSION_LABEL}${KERNEL_TYPE}/g" ./debian.master/changelog;
 
@@ -1002,7 +1002,7 @@ case $yno in
         ;&
     *)
         echo "*** Starting build... ✓";
-        NO_JEVENTS=1 fakeroot debian/rules binary-headers binary-generic binary-perarch;
+        NO_JEVENTS=1 NO_LIBTRACEEVENT=1 fakeroot debian/rules binary-headers binary-generic binary-perarch;
         ;;
 esac
 
@@ -1035,7 +1035,7 @@ echo "*** Finished installing kernel, cleaning up build directory... ✓";
 rm -rf ${KERNEL_BUILD_DIR};
 
 # To list your installed kernels: sudo update-grub2
-# To uninstall a kernel: sudo apt purge *6.6.1-060601+customidle-generic*
+# To uninstall a kernel: sudo apt purge *6.6.2-060602+customidle-generic*
 # Also, keep an eye out for the directories below as they build up over time.
 echo "ls -alh /usr/src"
 ls -alh /usr/src;
