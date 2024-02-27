c617bf97261d9685d67aed9f26ce09838d8371d6
Arvin Foroutan <arvin.foroutan@gmail.com>
2024-02-26 17:38:01 -0800

    Add graysky cpu patches for 6.7 and 6.8
    Source: https://github.com/graysky2/kernel_compiler_patch

--- build_kernel.sh
+++ build_kernel.sh
@@ -215,6 +215,9 @@ if [ ${KERNEL_BASE_VER} == "6.8" ]; then    # Latest rc
     echo "*** Copying and applying drm patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}-rc/futex-patches/*.patch .;
     patch -p1 < ./0001-futex-6.8-Add-entry-point-for-FUTEX_WAIT_MULTIPLE-op.patch;
+    echo "*** Copying and applying graysky cpu patches.. ✓";
+    cp -v ${CUSTOM_PATCH_PATH}/graysky/graysky-gcc-6.8-rc4+.patch .;
+    patch -p1 < ./graysky-gcc-6.8-rc4+.patch;
     echo "*** Copying and applying lucjan's xanmod patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}-rc/xanmod-patches-sep/*.patch .;
     if [ ${KERNEL_TYPE} != "rt" ]; then
@@ -268,8 +271,8 @@ elif [ ${KERNEL_BASE_VER} == "6.7" ]; then  # Latest stable
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/winesync-patches/*.patch .;
     patch -p1 < ./0001-winesync-Introduce-the-winesync-driver-and-character.patch;
     echo "*** Copying and applying graysky cpu patches.. ✓";
-    cp -v ${CUSTOM_PATCH_PATH}/graysky/graysky-gcc-5.17+.patch .;
-    patch -p1 < ./graysky-gcc-5.17+.patch;
+    cp -v ${CUSTOM_PATCH_PATH}/graysky/graysky-gcc-6.1.79-6.8-rc3.patch .;
+    patch -p1 < ./graysky-gcc-6.1.79-6.8-rc3.patch;
     echo "*** Copying and applying lucjan's xanmod patches.. ✓";
     cp -v ${LUCJAN_PATCH_PATH}/${KERNEL_BASE_VER}/xanmod-patches-sep/*.patch .;
     if [ ${KERNEL_TYPE} != "rt" ]; then
