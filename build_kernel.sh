387ecceb56082276fe3e52c4780edf672def7392
Arvin Foroutan <arvin.foroutan@gmail.com>
2024-10-19 11:23:34 -0700

    6.11.3 -> 6.11.4

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,8 +6,8 @@ set -euo pipefail
 
 KERNEL_MAJOR_VER=${KERNEL_MAJOR_VER:-"6"}
 KERNEL_BASE_VER=${KERNEL_BASE_VER:-"6.11"}
-KERNEL_PATCH_VER=${KERNEL_PATCH_VER:-"6.11.3"}
-KERNEL_SUB_VER=${KERNEL_SUB_VER:-"061103"}
+KERNEL_PATCH_VER=${KERNEL_PATCH_VER:-"6.11.4"}
+KERNEL_SUB_VER=${KERNEL_SUB_VER:-"061104"}
 KERNEL_TYPE=${KERNEL_TYPE:-"idle"}
 KERNEL_VERSION_LABEL=${KERNEL_VERSION_LABEL:-"custom"}
 
@@ -1039,13 +1039,14 @@ elif [ ${KERNEL_BASE_VER} == "5.4" ]; then  # LTS kernel, supported until 2025
     patch -p1 < ./0009-cpufreq-tunes-ondemand-and-conservative-governor-for.patch;
     patch -p1 < ./0010-scripts-disable-the-localversion-tag-of-a-git-repo.patch;
 fi
+# fi
 
 # Examples:
-# 6.11.3-061103+customidle-generic
-# 6.11.3-061103+customfull-generic
-# 6.11.3-061103+customrt-generic
+# 6.11.4-061104+customidle-generic
+# 6.11.4-061104+customfull-generic
+# 6.11.4-061104+customrt-generic
 # Note: A hyphen between label and type (e.g. customidle -> custom-idle) causes problems with some parsers
-# Because the final version name becomes: 6.11.3-061103+custom-idle-generic, so just keep it combined
+# Because the final version name becomes: 6.11.4-061104+custom-idle-generic, so just keep it combined
 echo "*** Updating version in changelog (necessary for Ubuntu)... ✓";
 sed -i "s/${KERNEL_SUB_VER}/${KERNEL_SUB_VER}+${KERNEL_VERSION_LABEL}${KERNEL_TYPE}/g" ./debian.master/changelog;
 
@@ -1181,7 +1182,7 @@ echo "*** Finished installing kernel, cleaning up build directory... ✓";
 rm -rf ${KERNEL_BUILD_DIR};
 
 # To list your installed kernels: sudo update-grub2
-# To uninstall a kernel: sudo apt purge *6.11.3-061103+customidle-generic*
+# To uninstall a kernel: sudo apt purge *6.11.4-061104+customidle-generic*
 # Also, keep an eye out for the directories below as they build up over time.
 echo "ls -alh /usr/src"
 ls -alh /usr/src;
